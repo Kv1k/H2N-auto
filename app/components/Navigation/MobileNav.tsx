92387c7ead37e553940f817144fae0c6808e4cd5
Kamil <Kam.nach@hotmail.com>
2025-02-05 21:16:52 +0100

    add alt

--- app/components/Navigation/MobileNav.tsx
+++ app/components/Navigation/MobileNav.tsx
@@ -19,7 +19,7 @@ const MobileNav = ({closeNav, showNav, mentionPage}: Props) => {
 
     return (
         <div className={`fixed ${navStyle} right-0 transition-all duration-500 left-0 top-0 bottom-0 h-[100vh] bg-[#000000e0] z-[1002]`}>
-            <Image src={ImCross} onClick={closeNav} className='absolute top-[2rem] right-[2rem] w-[2rem] h-[2rem] cursor-pointer'/>
+            <Image alt='' src={ImCross} onClick={closeNav} className='absolute top-[2rem] right-[2rem] w-[2rem] h-[2rem] cursor-pointer'/>
             <div className={`bg-[#35839d] ${navStyle} transition-all duration-500 delay-200 flex flex-col items-center justify-center w-[70%] h-[100%]`}>
                 <ul className='space-y-10'>
                     <li className='text-[25px] font-medium hover:text-yellow-400 text-white'>
